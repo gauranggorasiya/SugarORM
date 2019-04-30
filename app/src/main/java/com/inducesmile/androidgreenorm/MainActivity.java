622825af21000012f4338d67abfd3359b0589642
PC <pc@PCs-iMac-2.local>
2019-04-30 19:12:23 +0530

    Initial commit

---
+++ app/src/main/java/com/inducesmile/androidgreenorm/MainActivity.java
@@ -0,0 +1,35 @@
+package com.inducesmile.androidgreenorm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private CategoryPojo categoryPojo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        categoryPojo = new CategoryPojo();
+        categoryPojo.setImage("bulgar");
+        categoryPojo.setText("Bulgar");
+        categoryPojo.save();
+
+        recyclerView = (RecyclerView)findViewById(R.id.category_menu);
+        GridLayoutManager mGrid = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(mGrid);
+        recyclerView.setHasFixedSize(true);
+
+        List<CategoryPojo> imageSugar = CategoryPojo.listAll(CategoryPojo.class);
+
+        CategoryAdapter mAdapter = new CategoryAdapter(this, imageSugar);
+        recyclerView.setAdapter(mAdapter);
+    }
+}
